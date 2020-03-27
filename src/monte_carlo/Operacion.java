19ef593a4659dd67d0cf38c470efa4a8a3ad58af
Pablo <18240248@itleon.edu.mx>
2020-03-26 19:11:57 -0600

    Added Operacion class skeleton

---
+++ src/monte_carlo/Operacion.java
@@ -0,0 +1,36 @@
+package monte_carlo;
+
+public class Operacion {
+    float probabilidad;
+    String nombre;
+    LocalTime duracion;
+
+    public Operacion(String nombre, LocalTime duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalTime getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(LocalTime duracion) {
+        this.duracion = duracion;
+    }
+
+    public float getProbabilidad() {
+        return probabilidad;
+    }
+
+    public void setProbabilidad(float probabilidad) {
+        this.probabilidad = probabilidad;
+    }
+}
